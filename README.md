5eadd67e67f47fed6663d8a8d45ca85fa7d191cc
Val-Priad <valeriipriadchenko@gmail.com>
2025-09-02 21:09:02 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hello World with Spring 🍃
+The simplest program ever that logs hello world to the console.
---
+++ src/main/java/com/example/helloworldwithspring/Application.java
@@ -0,0 +1,46 @@
+package com.example.helloworldwithspring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+    public static void main(String[] args) {
+
+        printSurroundedMessage("Start");
+
+        SpringApplication.run(Application.class, args);
+
+        printSurroundedMessage("End");
+
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("\n\tHello World");
+    }
+
+    public static void printSurroundedMessage(String message) {
+        final int LINE_LENGTH = 40;
+        System.out.println(getLine(LINE_LENGTH, "red"));
+        System.out.println(getColoredString(message, "green"));
+        System.out.println(getLine(LINE_LENGTH, "red"));
+
+    }
+
+    public static String getLine(int length, String color) {
+        return getColoredString("-".repeat(length), color);
+
+    }
+
+    public static String getColoredString(String message, String color) {
+        final String reset = "\033[0m";
+
+        return switch (color) {
+            case "red" -> "\033[31m" + message + reset;
+            case "green" -> "\033[32m" + message + reset;
+            default -> message;
+        };
+    }
+}
